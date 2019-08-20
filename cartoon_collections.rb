882c458462f02c0f74bc2d1d1afcdaade21a28a3
favorable-device-5158 <blee2125@gmail.com>
2019-08-20 20:14:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,6 +15,6 @@ end
 def find_the_cheese(ingredients)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  return cheese if ingredients.find { |ingredient| cheeses.include?(ingredient)  }
+  ingredients.find { |ingredient| cheeses.include?(ingredient)  }
 
 end

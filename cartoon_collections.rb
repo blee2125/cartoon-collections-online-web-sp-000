d3ab544e470a1138d2018a9c76f3e97dcded6117
favorable-device-5158 <blee2125@gmail.com>
2019-08-20 19:17:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)# code an argument here
-  dwarves.each_with_index do |dwarf, index| 
+  dwarves.each_with_index do |dwarf, index|
     puts (index+1)". #{dwarf}"
   end
 end
